d1f6eedfb577d03a358767d644007aa8791ddfdb
sobebarali <sobebar_ali17@hotmail.com>
2024-12-09 05:15:27 +0530

    Update ESLint configuration, add new components.json and package-lock.json, and enhance Tailwind CSS configuration
    - Disabled specific TypeScript ESLint rules in `.eslintrc.cjs`.
    - Added `components.json` for component configuration with Tailwind CSS.
    - Created `package-lock.json` to lock dependencies and ensure consistent installs.
    - Updated `.gitignore` to include `/scripts` directory.
    - Enhanced Tailwind CSS configuration in `tailwind.config.ts` with new color and border radius settings, and added `tailwindcss-animate` plugin.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,7 +36,10 @@ const config = {
           "attributes": false
         }
       }
-    ]
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off"
   }
 }
 module.exports = config;
